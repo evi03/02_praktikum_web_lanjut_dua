070645aee1d5968402f11931117fbbc76ef8ee5c
= evi03 <= evi.nzbk@gmail.com>
2023-02-23 13:41:22 +0700

    02_Praktikum_web_lanjut_dua_Evi

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    // public function index(){
+    //     return 'Selamat Datang controller';
+    // }
+
+    // public function about(){
+    //     return "Nama Evi Amalia Midfia Dengan Nim 2141720030";
+    // }
+
+    // public function articles($id){
+    //     return "Halaman Artikel dengan id $id";
+    // }
+}
+
